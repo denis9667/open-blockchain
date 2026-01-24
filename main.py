15c370dee3ac8d4129d6f46baad834e6da4da175
denis9667 <1bannikovdenis23021987@gmail.com>
2026-01-24 18:51:27 +0300

    added admin-panel

--- main.py
+++ main.py
@@ -6,7 +6,15 @@ password = "admin"
 log = ""
 pas = ""
 
-def login():
+def adminpanel():
+    print("#############open-blockchain##########")
+    print("###############admin-panel############")
+    print("# 1. start open-blockchain           #")
+    print("# 2. settings                        #")
+    print("# 3. quit admin-panel                #")
+    print("######################################")
+                                                   
+def login():                                        
     print("#############open-blockchain##########")
     print("to start/manage open-blockchain log in")
     print("######################################")
@@ -16,6 +24,8 @@ def login():
         pas = input("password:")
         if pas == password:
             print("login successful!")
+            print("")
+            adminpanel()
         else:
             print("wrong")
             login()
