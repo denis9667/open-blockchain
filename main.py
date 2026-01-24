2d55615684d48ee9ed6ac613beadbd8ff16fac36
denis9667 <1bannikovdenis23021987@gmail.com>
2026-01-24 18:13:28 +0300

    added login

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import os
 import numpy as num
 
+user = "admin"
+password = "admin"
+log = ""
+pas = ""
+
+def login():
+    print("#############open-blockchain##########")
+    print("to start/manage open-blockchain log in")
+    print("######################################")
+    print("")
+    log = input("username:")
+    if log == user:
+        pas = input("password:")
+        if pas == password:
+            print("login successful!")
+        else:
+            print("wrong")
+            login()
+    else:
+        print("wrong")
+        login()
+
 print("starting open-blockchain")
 print("|||")
 print("|||")
 print("|||")
+login()
+
+
